22eb64aacf3fb0b303344c7ba0e158269c9e9334
BFieguth <bfieguth@uoguelph.ca>
2024-09-13 07:53:57 -0400

    feat: use sys.settrace to capture intermediates

--- base_mechanistic_model.py
+++ base_mechanistic_model.py
@@ -1,8 +1,36 @@
 import abc
+import sys
+from functools import wraps
 
 import pandas as pd
 
-class BaseMechanisticModel():
+class BaseMechanisticModel(abc.ABC):
+    def __init__(self):
+        self.intermediates = {}
+   
+    def trace_func(self, frame, event, arg):
+        """Tracing function to capture local variables at return from the model."""
+        if event == "return" and frame.f_code.co_name == "model":
+            self.intermediates = frame.f_locals.copy()
+        return self.trace_func
+
+    def start_tracing(self):
+        """Enable tracing for the model method."""
+        sys.settrace(self.trace_func)
+
+    def stop_tracing(self):
+        """Disable tracing."""
+        sys.settrace(None)
+
+    def __filter_intermediates(self):
+        """Private method to filter captured locals based on outputs."""
+        return {var: self.intermediates.get(var) for var in self.outputs if var in self.intermediates}
+
+    @abc.abstractmethod
+    def model(self, state_vars, t):
+        """User-defined model function. Must return only differentials."""
+        pass
+
     def run_model(
         self, 
         equation: str, 
@@ -12,10 +40,12 @@ class BaseMechanisticModel():
         integ_interval, 
         prev_output=None
     ):
+
         if equation in []:
             pass # Run solve_ivp
 
         elif equation == "RK4":
+            self.start_tracing()
             results = self.runge_kutta_4th_order(
                 t_span=t_span,
                 y0=y0,
@@ -23,9 +53,10 @@ class BaseMechanisticModel():
                 integ_interval=integ_interval,
                 prev_output=prev_output
             )
-            return results
+    
+        self.stop_tracing()
+        return results
 
-# TODO deal with variable_returns
     def runge_kutta_4th_order(
         self, 
         t_span, 
@@ -34,7 +65,7 @@ class BaseMechanisticModel():
         integ_interval, 
         prev_output = None
     ):
-        """Main function that runs the refactored Runge-Kutta algorithm with a generator."""
+        """Main function that runs the Runge-Kutta algorithm."""
         
         def interval_generator():
             """Generator function to dynamically yield intervals."""
@@ -65,30 +96,30 @@ class BaseMechanisticModel():
             """Runge-Kutta step that computes new state variables."""
             slopes = []  # To store the slopes for the RK4 method
             start = []  # Stores initial values for the RK4 integration
+            
+            half_interval = integ_interval / 2
+            sixth_interval = integ_interval / 6
 
             for n in range(4):
-                differential_return, variable_returns = self.model(
-                    state_vars=state_vars, t=t
-                )
+                differential_return = self.model(state_vars=state_vars, t=t)                  
                 slopes.append(differential_return)
 
                 for svno in range(len(state_vars)):
-                    match n:
-                        case 0:
-                            start.append(state_vars[svno])
-                            newStateVar = start[svno] + integ_interval * slopes[n][svno] / 2
-                        case 1:
-                            newStateVar = start[svno] + integ_interval * slopes[n][svno] / 2
-                        case 2:
-                            newStateVar = start[svno] + integ_interval * slopes[n][svno]
-                        case 3:
-                            newStateVar = (start[svno] + integ_interval / 6 *
-                                        (slopes[0][svno] + 2 * slopes[1][svno] +
-                                            2 * slopes[2][svno] + slopes[3][svno]))
+                    if n == 0:
+                        start.append(state_vars[svno])
+                        newStateVar = start[svno] + half_interval * slopes[n][svno]
+                    elif n == 1:
+                        newStateVar = start[svno] + half_interval * slopes[n][svno]
+                    elif n == 2:
+                        newStateVar = start[svno] + integ_interval * slopes[n][svno]
+                    else:
+                        newStateVar = (start[svno] + sixth_interval *
+                                    (slopes[0][svno] + 2 * slopes[1][svno] +
+                                        2 * slopes[2][svno] + slopes[3][svno]))
 
                     state_vars[svno] = newStateVar
 
-            return state_vars, variable_returns
+            return state_vars
 
         ### Main Function ###
         model_results = []
@@ -99,17 +130,12 @@ class BaseMechanisticModel():
 
         # Iterate over the generated intervals and apply the Runge-Kutta steps
         for t, append_result in interval_gen:
-            state_vars, variable_returns = runge_kutta_step(state_vars, t)            
+            state_vars = runge_kutta_step(state_vars, t)            
             if append_result:
-                model_results.append(variable_returns.copy())
+                model_results.append(self.__filter_intermediates())
     
         return model_results
-
-
+    
     # TODO Need to standardize the output from run model 
     # TODO Add methods to work with standardized output
      
-    @abc.abstractmethod
-    def model(self):
-        """User defined model function."""
-        pass
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import pandas as pd
 
 from base_mechanistic_model import BaseMechanisticModel
@@ -9,7 +11,7 @@ class DemoModel(BaseMechanisticModel):
         self.YBAB = YBAB
         self.vol = vol        
         self.outputs = outputs
-
+    
     def model(self, state_vars, t):
         kAB = self.kAB
         kBO = self.kBO
@@ -31,18 +33,7 @@ class DemoModel(BaseMechanisticModel):
         dAdt = -UAAB
         dBdt = PBAB - UBBO
 
-        # The model equations are written inside the dynamic function
-
-        # The dynamic function must end with the following code to format the outputs
-        # The differential and variable returns are handled sepperately to allow the 4th-order Runge-Kutta equation to run    
-        # Format data for return # 
-        differential_return = [dAdt, dBdt] # same number and order as stateVars
-        local_variables = locals()
-        # Store local variables 
-        variable_returns = [local_variables.get(variable_name) for variable_name in self.outputs]
-        # Create list of variables to return
-
-        return differential_return, variable_returns
+        return [dAdt, dBdt]
 
 
 if __name__ == "__main__":
@@ -51,6 +42,8 @@ if __name__ == "__main__":
         df = pd.DataFrame(data, columns=columns)
         return df
 
+    print("Starting model run...")
+    start_time = time.time()
 
     demo = DemoModel(
         kAB=0.42, kBO=0.03, YBAB=1.0, vol=1.0, 
@@ -60,11 +53,19 @@ if __name__ == "__main__":
         "RK4", t_span=(0, 120), y0=[3.811004739069482, 4.473254058347129],
         t_eval=10, integ_interval=0.001
         )
+    
+    elapsed_time = time.time() - start_time
+    print(f"First model run completed in {elapsed_time:.4f} seconds.")
+
     df = convert_list_to_dataframe(result)
     print(df)
 
     # Test restarting from prev_output
     new_stateVars =  df.iloc[-1, df.columns.isin(['A', 'B'])].tolist()
+    
+    print("\nStarting second model run...")
+    start_time = time.time()
+
     demo = DemoModel(
         kAB=0.5, kBO=0.03, YBAB=1.0, vol=1.0, 
         outputs=['t', 'A', 'B', 'concA', 'concB', 'dAdt']
@@ -73,5 +74,9 @@ if __name__ == "__main__":
         "RK4", t_span=(120, 220), y0=new_stateVars, t_eval=10, 
         integ_interval=0.01, prev_output=df
         )
+
+    elapsed_time = time.time() - start_time
+    print(f"Second model run completed in {elapsed_time:.4f} seconds.")
+
     df2 = convert_list_to_dataframe(result2)
     print(df2)
