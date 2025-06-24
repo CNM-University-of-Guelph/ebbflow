ed5fb51bc32727db4441ea5bb979c38a436a84d2
BFieguth <bfieguth@uoguelph.ca>
2025-06-24 14:29:48 -0400

    feat: allow constants to be passed as ic to AcslLib.integ()

--- src/ebbflow/acsl/visitors/statevar_collector.py
+++ src/ebbflow/acsl/visitors/statevar_collector.py
@@ -10,8 +10,13 @@ class StatevarCollector(ast.NodeVisitor):
             isinstance(node.value.func, ast.Attribute) and
             node.value.func.attr == "integ"
             ):
-            if len(node.targets) == 1:
+            # Variable passed as ic
+            if len(node.targets) == 1 and isinstance(node.value.args[1], ast.Name):
                 self.integ_calls[node.targets[0].id] = node.value.args[1].id
+
+            # Constant passed as ic 
+            elif len(node.targets) == 1 and isinstance(node.value.args[1], ast.Constant):
+                self.integ_calls[node.targets[0].id] = node.value.args[1].value
 
             else:
                 raise ValueError(
