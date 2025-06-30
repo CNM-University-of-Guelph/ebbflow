00effa6789028a2052432d29ebeee32f2a79c1a9
BFieguth <bfieguth@uoguelph.ca>
2025-06-30 11:28:54 -0400

    docs: document and lint AcslBuild

--- src/ebbflow/acsl/build/acsl_build.py
+++ src/ebbflow/acsl/build/acsl_build.py
@@ -1,7 +1,8 @@
-"""Take the user defined ACSL model and build an executable model.
+"""Build an executable AcslRun instance from the user-defined sections of an 
+ACSL model.
 """
 import ast
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Optional, List
 
 from ebbflow.acsl.build.ast_visitors.constant_manager import ConstantManager
 from ebbflow.acsl.build.ast_visitors.statevar_collector import StatevarCollector
@@ -13,15 +14,31 @@ from ebbflow.acsl.build.ast_visitors.integ_function_creator import IntegFunction
 from ebbflow.acsl.integration.integration_manager import IntegrationManager
 
 class AcslBuild:
-    """Build the AcslSection objects and configure AcslRun instance.
+    """Create an executable AcslRun instance from the user-defined sections of 
+    an ACSL model. An ACSL model inherits from the Acsl class and is defined by
+    a set of user-defined functions.
+    
+    Parameters
+    ----------
+    section_trees : Dict[str, Tuple[ast.AST, Dict]]
+        A dictionary of section name, AST tree and metadata.
+    initial_scope : Tuple[str, Dict]
+        A tuple containing the scope of the INITIAL section.
+    TSTP : float | int
+        The stop time.
+    CINT : int, optional
+        The communication interval used for reporting. This is also used
+        to determine the integration step size.
+    report : list, optional
+        A list of variable names to include in the ouput.
     """
     def __init__(
             self,
             section_trees: Dict[str, Tuple[ast.AST, Dict]],
             initial_scope: Tuple[str, Dict],
-            TSTP: float,
-            CINT: int = None,
-            report: list = None
+            TSTP: float | int, # pylint: disable=invalid-name
+            CINT: Optional[float | int] = None, # pylint: disable=invalid-name
+            report: Optional[List[str]] = None
         ):
         self.section_trees = section_trees
         self.constants = {}
@@ -31,8 +48,8 @@ class AcslBuild:
             "NSTP": None,
             "MAXT": None,
         }
-        self.CINT = CINT # NOTE: Passed in from Acsl.run() method
-        self.TSTP = TSTP
+        self.CINT = CINT # pylint: disable=invalid-name
+        self.TSTP = TSTP # pylint: disable=invalid-name
         self.constant_manager = ConstantManager(initial_scope)
         self.statevar_collector = StatevarCollector()
         self.sorter = AcslSort()
@@ -41,6 +58,19 @@ class AcslBuild:
         self.variables_to_report = report
 
     def build(self):
+        """Build the executable AcslRun instance.
+        
+        This method will collect the constants, statevars, integration settings
+        and create the executables for the sections. Calls to procedural blocks
+        are added to the respective executable. Functions are created for the 
+        differential equation of each state variable to be used by the 
+        integration routine.
+
+        Returns
+        -------
+        AcslRun
+            An AcslRun instance that will exercise the model.
+        """
         # Collect constants
         for section_name, tree, _ in self._iterate("collect_constants"):
             self.constant_manager.collect(section_name, tree)
@@ -58,17 +88,20 @@ class AcslBuild:
             ), metadata)
 
         # Collect integration settings from DYNAMIC section
-        self.integration_settings_collector.visit(self.section_trees.get("DYNAMIC")[0])
+        self.integration_settings_collector.visit(
+            self.section_trees.get("DYNAMIC")[0]
+        )
         self.integration_settings = self.integration_settings_collector.settings
-        
+
         # Set CINT from constants
         self.CINT = self.constants.get("CINT", self.CINT)
         if self.CINT is None:
             self.CINT = self.integration_settings.get("CINT", None)
         if self.CINT is None or not isinstance(self.CINT, int):
-            self.CINT = 1  # NOTE: set default for cases where CINT is defined in Dynamic
-                           # Need to be able to initalize the integration routine which means IntegrationManager
-                           # needs to be initialized with a value
+            self.CINT = 1
+            # NOTE: set default for cases where CINT is defined in Dynamic
+            # Need to be able to initalize the integration routine which means
+            # IntegrationManager needs to be initialized with a value
 
         # Create executables for integration manager
         derivative_functions = {}
@@ -99,10 +132,26 @@ class AcslBuild:
             section_functions[section_name].remove_self_calls()
             section_functions[section_name].create_executable()
 
-        dynamic_func = section_functions.get("DYNAMIC").executable_func if section_functions.get("DYNAMIC") else None
-        derivative_func = section_functions.get("DERIVATIVE").executable_func if section_functions.get("DERIVATIVE") else None
-        discrete_func = section_functions.get("DISCRETE").executable_func if section_functions.get("DISCRETE") else None
-        terminal_func = section_functions.get("TERMINAL").executable_func if section_functions.get("TERMINAL") else None
+        dynamic_func = (
+            section_functions.get("DYNAMIC").executable_func
+            if section_functions.get("DYNAMIC")
+            else None
+        )
+        derivative_func = (
+            section_functions.get("DERIVATIVE").executable_func
+            if section_functions.get("DERIVATIVE")
+            else None
+        )
+        discrete_func = (
+            section_functions.get("DISCRETE").executable_func
+            if section_functions.get("DISCRETE")
+            else None
+        )
+        terminal_func = (
+            section_functions.get("TERMINAL").executable_func
+            if section_functions.get("TERMINAL")
+            else None
+        )
 
         return AcslRun(
             TSTP=self.TSTP,
@@ -117,11 +166,25 @@ class AcslBuild:
             terminal=terminal_func,
         )
 
-    def _iterate(self, filter: str):
-        """
-        Iterate over the section trees and yield the section name, tree, and metadata
-        if the section name matches the filter.
+    def _iterate(self, attribute: str):
+        """Iterate over the section trees and yield the section name, tree, and
+        metadata if the section name matches the attribute name.
+
+        Parameters
+        ----------
+        attribute : str
+            The attribute name to filter by.
+
+        Yields
+        ------
+        section_name : str
+            The name of the section.
+        tree : ast.AST
+            The AST tree of the section.
+        metadata : Dict
+            The metadata of the section with instruction for how to process the
+            section tree.
         """
         for section_name, (tree, metadata) in self.section_trees.items():
-            if metadata.get(filter):
+            if metadata.get(attribute):
                 yield section_name, tree, metadata
